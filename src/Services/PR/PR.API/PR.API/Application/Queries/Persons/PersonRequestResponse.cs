b8addb7166d80171a6e76ab292b23db8cd4661ab
DimitrisDimitrako <dimdimitrako@gmail.com>
2022-06-13 01:16:28 +0300

    PR : Friends

--- src/Services/PR/PR.API/PR.API/Application/Queries/Persons/PersonRequestResponse.cs
+++ src/Services/PR/PR.API/PR.API/Application/Queries/Persons/PersonRequestResponse.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using PR.API.Application.Queries.FriendRequests;
-using PR.Domain.AggregatesModel.FriendRequestAggregate;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PR.API.Application.Queries.Persons;
 
 public class PersonRequestResponse
 {
-	public PersonRequestResponse(int id, string identityGuid, IReadOnlyCollection<FriendResponse> friendshipsSent,
+	public PersonRequestResponse(int id, string identityGuid, List<FriendResponse> friendshipsSent,
 		List<FriendResponse> friendshipsReceived)
 	{
 		Id = id;
 		IdentityGuid = identityGuid;
 		FriendshipsSent = friendshipsSent;
 		FriendshipsReceived = friendshipsReceived;
+		Friends = friendshipsReceived.Concat(friendshipsSent);
 	}
 
 	public int Id { get; }
 	public string IdentityGuid { get; }
 
 	public IReadOnlyCollection<FriendResponse> FriendshipsSent { get; }
-	public IReadOnlyCollection<FriendResponse> FriendshipsReceived { get; }
+	public IList FriendshipsReceived { get; }
+	public IEnumerable<FriendResponse> Friends { get; set; }
 }
